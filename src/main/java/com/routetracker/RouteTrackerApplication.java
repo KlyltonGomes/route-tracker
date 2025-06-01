d0c25253f3517f46ae677e50a536f5f91222dda3
klyltongomes <klyltongomesdesouza@MacBook-Pro-de-klylton.local>
2025-05-31 23:23:01 -0300

    Criado as Config application.properties test_dev_prod.

--- src/main/java/com/routetracker/RouteTrackerApplication.java
+++ src/main/java/com/routetracker/RouteTrackerApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RouteTrackerApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Start project");
 		SpringApplication.run(RouteTrackerApplication.class, args);
+		System.out.println("Let`s Go!!");
 	}
 
 }
