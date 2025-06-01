ee83b3523ce9a36bdad3ea9261e9b538e73b15a7
klyltongomes <klyltongomesdesouza@MacBook-Pro-de-klylton.local>
2025-05-31 21:52:15 -0300

    Projeto inicial - Route Tracker

---
+++ src/test/java/com/routetracker/RouteTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.routetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
